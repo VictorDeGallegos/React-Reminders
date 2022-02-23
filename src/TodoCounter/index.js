9825c27fab0956014e15bde475991128f1c5ca7a
Victor Gallegos <316160456@ciencias.unam.mx>
2022-02-23 11:53:20 -0600

    Mejorando TodoCounter

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -4,10 +4,18 @@ import './TodoCounter.css';
 
 function TodoCounter() {
   const { totalTodos, completedTodos } = React.useContext(TodoContext);
-
+  React.useEffect(() => {
+    totalTodos - completedTodos > 0
+      ? (document.title = `${totalTodos - completedTodos} ${totalTodos - completedTodos === 1 ? 'GOAL pendiente' : 'GOALS pendientes'}`)
+      : (document.title = `GOALS`);
+  }, [totalTodos, completedTodos]);
   return (
     <h2 className="TodoCounter">
-      Han completado {completedTodos} de {totalTodos} GOALS
+      {(completedTodos === 0) & (totalTodos === 0)
+        ? 'No  tienen GOALS pendientes'
+        : totalTodos === completedTodos
+        ? 'Han completado todos sus GOALS 😍'
+        : `Han completado ${completedTodos} de ${totalTodos} Goals`}
     </h2>
   );
 }
